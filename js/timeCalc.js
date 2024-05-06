9b23eff0e3188180e2d03c9f0ee505c1cd2e105c
Owehttamy <owehttamyplays@yahoo.com>
2024-05-06 16:23:00 -0500

    v2.0
    - Changed the text boxes to time fields
    - Consistent formatting
    - Include max of four hours
    - Removed the unnecessary buttons
    - Everything is better and cleaner

--- js/timeCalc.js
+++ js/timeCalc.js
@@ -1,26 +1,50 @@
-// Function to calculate minutes between two times
+let duration="", chunks="", myres="";
+
+var startTimeInput = document.getElementById("start");
+var endTimeInput = document.getElementById("end");
+
+document.addEventListener("DOMContentLoaded", function() {
+    run();
+});
+
+startTimeInput.addEventListener(
+    "input",
+    () => {
+      run();
+    },
+    false,
+);
+
+endTimeInput.addEventListener(
+    "input",
+    () => {
+        run();
+    },
+    false,
+);
+
+function run() {
+    var start = startTimeInput.value;
+    var end = endTimeInput.value;
+    duration = calcMinutes(start, end);
+    let [chunks, decimal] = calcChunks(duration).split("|");
+    document.getElementById('min').innerHTML = duration;
+    document.getElementById('cost').innerHTML = chunks;
+    document.getElementById('dec').innerHTML = decimal;
+}
+
+
 function calcMinutes(start, end) {
-    // Splitting hours and minutes from the input strings
     let [startHours, startMinutes] = start.split(":").map(Number);
     let [endHours, endMinutes] = end.split(":").map(Number);
 
-    // Fix AM/PM issue
-    if (endHours < startHours) {
-        endHours += 12;
-    }
-
-    // Calculating the total minutes for start and end times
     let startTotalMinutes = startHours * 60 + startMinutes;
     let endTotalMinutes = endHours * 60 + endMinutes;
 
-
-    // Calculating the difference in minutes
     let minutesDifference = endTotalMinutes - startTotalMinutes;
-
     return minutesDifference;
 }
 
-// Function to calculate additional price
 function calcChunks(duration) {
     let current = duration - 60;
     let first_hour = "Base + $";
@@ -31,43 +55,17 @@ function calcChunks(duration) {
         count++;
     }
 
+    if (count > 16) {
+        count = 16;
+    }
+
     let total_fifteen = count * 5;
 
     let dec60 = ((count*15 + 60)/60);
 
     let output = first_hour.concat(total_fifteen);
 
-    output = output.concat(" | ", dec60);
+    output = output.concat("|", dec60);
 
     return output;
-}
-
-// Result Formation
-function express(duration, chunks) {
-    let res;
-    res = duration + " | " + chunks;
-    return res.toString();
-}
-
-let duration="", chunks="", myres="";
-document.getElementById('btnEql').onclick = function() {
-    var start = document.getElementById("start").value;
-    var end = document.getElementById("end").value;
-    // Calling the calcMinutes function and displaying the result
-    duration = calcMinutes(start, end);
-    chunks = calcChunks(duration);
-    myres = express(duration, chunks);
-    document.getElementById('res').innerHTML = myres;
-}
-
-function showex() {
-    document.getElementById('res').innerHTML = duration+chunks;
-}
-
-
-document.getElementById('btnClr').onclick = function() {
-    myres = "";
-    duration = "";
-    chunks = "";
-    document.getElementById('res').innerHTML = myres;
 }
